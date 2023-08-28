f2c4e9f2e29c2a66c3a608505ee86769e77c15aa
ccpowers <katypm@gmail.com>
2023-08-28 08:48:45 -0400

    ud client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
 
 function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [input, setInput] = useState<string>('');
+
+  useEffect(() => {
+    socket.on('server chat message', (msg: string) => {
+      console.log('got msg', msg);
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+
+    return () => {
+      console.log('Disconnecting from socket');
+      //socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (input.trim()) {
+      console.log('emitting client chat message');
+      socket.emit('client chat message', input);
+      setInput('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Type your message..."
+      />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
 
 export default App;
+
